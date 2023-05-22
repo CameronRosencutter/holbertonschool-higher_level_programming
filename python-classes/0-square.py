34c63862796755b156d8ee82ae08b1a525f480b2
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-05-22 06:34:57 -0700

    push

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Display a class Square."""
+"""Write an empty class Square that defines a square:"""
+
 
 class Square:
-        """Represent a Square."""
+        """This class defines a square"""
             pass
