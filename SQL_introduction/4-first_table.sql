59aeb14006f92dc780e19296e98da5a36160d0c5
Cameron <120847793+CameronRosencutter@users.noreply.github.com>
2023-06-25 16:02:49 -0500

    push

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- SQL script that creates a table called first_table in the current database in your MySQL server.
+-- description: first_table with,  id INT,  name VARCHAR(256)
+CREATE table if NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(150)
+);
